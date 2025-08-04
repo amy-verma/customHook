83e8d32260da1e9ec5a89fef0a72bc026c309588
amy-verma <amitjsr8786@gmail.com>
2025-08-04 13:16:09 +0530

    custom-hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
+import useCount from "./useCount"
 
-import './App.css'
-import useCount from './useCount'
+const App=()=>{
 
-function App() {
- const [count,handleIncrement,handleDecrement]=useCount()
-
+  const [count,handleIncrement,handleDecrement]=useCount()
   return (
-    <>
-    <p>{count}</p>
-     <button onClick={handleIncrement}>Increment</button>
-  
-     <button onClick={handleDecrement} >Decrement</button>
-    </>
+    <div>
+      <p>{count}</p>
+    <button onClick={handleIncrement}>Increment</button>
+    <button onClick={handleDecrement}>Decrement</button>
+    </div>
   )
 }
-
 export default App
+// import './App.css'
+// import useCount from './useCount'
+
+// function App() {
+//  const [count,handleIncrement,handleDecrement]=useCount()
+
+//   return (
+//     <>
+//     <p>{count}</p>
+//      <button onClick={handleIncrement}>Increment</button>
+  
+//      <button onClick={handleDecrement} >Decrement</button>
+//     </>
+//   )
+// }
+
+// export default App
--- src/useCount.jsx
+++ src/useCount.jsx
@@ -2,17 +2,32 @@ import { useState } from "react"
 
 const useCount=()=>{
     const [count,setCount]=useState(0)
-
-
     const handleIncrement=()=>{
         setCount((prev)=>prev+1)
     }
-
     const handleDecrement=()=>{
         setCount((prev)=>prev-1)
     }
-
     return [count,handleIncrement,handleDecrement]
 }
 
-export default useCount;
+export default useCount
+
+// import { useState } from "react"
+
+// const useCount=()=>{
+//     const [count,setCount]=useState(0)
+
+
+//     const handleIncrement=()=>{
+//         setCount((prev)=>prev+1)
+//     }
+
+//     const handleDecrement=()=>{
+//         setCount((prev)=>prev-1)
+//     }
+
+//     return [count,handleIncrement,handleDecrement]
+// }
+
+// export default useCount;
